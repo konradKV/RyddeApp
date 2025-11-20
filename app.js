fae1d00b7a6f99bc00f0276cf1aca5505389ef0f
koneradio2 <you@example.com>
2025-11-20 21:24:11 +0100

    Tuff Isak Brun
    Co-authored-by: isakbh <isak@brunhenriksen.net>

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-app.listen(6767);
 const sqlite3 = require("sqlite3").verbose();
 let sql;
 
+// rot
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// database :solbriller emotikon:
 const db = new sqlite3.Database("./ryddeApp", sqlite3.OPEN_READWRITE, (err) => {
   if (err) return console.error(err.message);
 });
 
+// get request for getTasks i
 app.get("/getTasks", (req, res) => {
   sql = "SELECT * FROM task";
   db.all(sql, [], (err, rows) => {
@@ -20,3 +22,6 @@ app.get("/getTasks", (req, res) => {
     res.json(rows);
   });
 });
+
+// server listener på port 6767 (http://localhost:6767) -konrad
+app.listen(6767);
